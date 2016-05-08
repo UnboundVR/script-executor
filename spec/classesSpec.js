c18f7b409f44ba5818387be07585fd3df4fbaefa
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-05-08 20:08:43 -0300

    loading classes sequentially

--- spec/classesSpec.js
+++ spec/classesSpec.js
@@ -1,16 +1,25 @@
 import Classes from '../src/classes';
 import sinon from 'sinon';
-import jasmineSinon from 'jasmine-sinon';
 
 let mockLoader;
 
 const id = 'someId';
+const otherId = 'someOtherId';
 const code = 'someCode';
+const otherCode = 'someOtherCode';
 const ctor = function() {};
 const obj = {};
+const promises = [];
 
 function setup(loadSuccess, resultIsFunc) {
   mockLoader = sinon.stub().withArgs(code).returns(loadSuccess ? Promise.resolve(resultIsFunc ? ctor : obj) : Promise.reject('some error'));
+  return new Classes(mockLoader);
+}
+
+function setupSequential() {
+  mockLoader = (code) => new Promise((resolve, reject) => {
+    promises.push({resolve, reject});
+  });
   return new Classes(mockLoader);
 }
 
@@ -21,6 +30,22 @@ describe('Classes', () => {
 
     expect(classes.get(id)).toBe(ctor);
     done();
+  });
+
+  it('should load sequentially', async (done) => {
+    let classes = setupSequential(true, true);
+
+    let firstPromise = classes.load(id, code);
+    let secondPromise = classes.load(otherId, otherCode);
+
+    expect(promises.length).toBe(1);
+
+    firstPromise.then(() => {
+      expect(promises.length).toBe(2);
+      done();
+    });
+
+    promises[0].resolve(ctor);
   });
 
   it('should throw and not register when loader returns not a function', async (done) => {
--- src/classes.js
+++ src/classes.js
@@ -1,11 +1,14 @@
+import Queue from 'promise-queue';
+
 export default class Classes {
   constructor(loader) {
     this._load = loader;
     this.classes = {};
+    this.queue = new Queue(1, Infinity);
   }
 
   async load(id, code) {
-    let ctor = await this._load(code);
+    let ctor = await this.queue.add(() => this._load(code));
 
     if(typeof ctor != 'function') {
       throw new Error('Code must declare a constructor - therefore it must be a function');
